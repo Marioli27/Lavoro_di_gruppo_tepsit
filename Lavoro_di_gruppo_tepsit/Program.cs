44765b3af06271285fd80f459dd89b6b136862f3
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 08:29:20 +0100

    Add project files.

---
+++ Lavoro_di_gruppo_tepsit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lavoro_di_gruppo_tepsit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
